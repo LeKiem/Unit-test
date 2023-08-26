85c583e27e8069bf3498cc3a83c4728bc26f2cf2
LeKiem <levankiemdev@gmail.com>
2023-08-26 15:03:42 +0700

    findMostFrequentNumber(numberList)

---
+++ practice/b9.js
@@ -0,0 +1,19 @@
+export function findMostFrequentNumber(numberList) {
+  if (!Array.isArray(numberList) || numberList.length === 0) return undefined;
+
+  const statistics = {};
+  for (let i = 0; i < numberList.length; i++) {
+    const number = numberList[i];
+    statistics[number] =
+      statistics[number] === undefined ? 1 : statistics[number] + 1;
+  }
+
+  let maxKey = undefined;
+  for (const key in statistics) {
+    if (maxKey === undefined || statistics[key] > statistics[maxKey]) {
+      maxKey = key;
+    }
+  }
+
+  return Number.parseInt(maxKey);
+}
